b6b50ec8a0e618eace5382f597a2ea5d6653e59e
Ashraf Hamadi <jask_ashraf@hotmail.com>
2021-06-06 03:04:11 +0200

    My first commit

---
+++ server/index.js
@@ -0,0 +1,48 @@
+/* Importera */
+var express = require("express");
+var bodyParser = require("body-parser");
+var cors = require('cors');
+var path = require("path");
+var mongoose = require("mongoose");
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+// anslut till databas
+//mongoose.connect("mongodb://localhost:27017/courses", { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Skapa instans av express
+var app = express();
+
+//Middleware. Gör webbtjänsterna tillgängliga även från andra domäner
+app.all('/*', function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	res.header("Access-Control-Allow-Methods", "GET,PUT,PATCH,POST,DELETE");
+	next();
+});
+
+// Body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded( { extended: false}));
+app.use(cors());
+
+const posts = require('./routes/api/posts');
+
+app.use('/api/posts', posts);
+
+// Handle Production
+if(process.env.NODE_ENV === 'production'){
+    // Static folder
+    app.use(express.static(__dirname + '/public/'));
+
+    // Handle SPA
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, function(){
+    console.log("Servern är startad på port " + port);
+});
